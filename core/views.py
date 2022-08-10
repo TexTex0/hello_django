b7b2c4bae7d480c68aa8ec63c9fdbed1d6d9936f
gaby <gaby_potencia@hotmail.com>
2022-08-09 21:54:53 -0300

    atualização primeira aplicação django, retornando um 'Hello World'

--- core/views.py
+++ core/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def hello(request, nome, idade):
-    return HttpResponse('<h1>Hello, {} de {} anos!</h1>'.format(nome, idade))
+    return HttpResponse('<h1>Hello {} de {} anos</h1>'.format(nome, idade))
